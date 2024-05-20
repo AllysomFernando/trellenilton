2e159a65798ff466daaf7cb2d8b99ebf909ccca2
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-05-20 15:30:56 -0300

    feat: adding deleteBoard at IBoardRepository

--- src/domain/contracts/board-repository.ts
+++ src/domain/contracts/board-repository.ts
@@ -1,7 +1,7 @@
 import type { Board } from "../entities/board";
 
-
 export interface IBoardRepository {
-  loadAllBoards: () => Promise<Board[]>
-  createBoard: (board: Board) => Promise<Board>
+  loadAllBoards: () => Promise<Board[]>;
+  createBoard: (board: Board) => Promise<Board>;
+  deleteBoard: (id: string) => Promise<void>;
 }
