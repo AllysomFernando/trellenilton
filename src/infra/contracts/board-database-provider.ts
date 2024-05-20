e2027ac821aaa8e1a123c866e4258c1a61f00d88
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-05-20 17:27:17 -0300

    feat: adding deleteBoard at board-database-provider

--- src/infra/contracts/board-database-provider.ts
+++ src/infra/contracts/board-database-provider.ts
@@ -1,4 +1,5 @@
 export interface IBoardDatabaseProvider {
-  loadAllBoards: () => Promise<any[]>
-  createBoard: (board: any) => Promise<any> 
+  loadAllBoards: () => Promise<any[]>;
+  createBoard: (board: any) => Promise<any>;
+  deleteBoard: (board: any, id: string) => Promise<any>;
 }
