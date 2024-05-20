6ac9ac032b98eef7afb98c0b58ff9d6662644b94
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-05-20 17:27:56 -0300

    feat: adding deleteBoard at drizzle-board-provider

--- src/infra/database/providers/drizzle-board-provider.ts
+++ src/infra/database/providers/drizzle-board-provider.ts
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm";
 import { db } from "../../../main/drizzle";
 import type { IBoardDatabaseProvider } from "../../contracts/board-database-provider";
 
@@ -5,16 +6,24 @@ export class DrizzleBoardProvider implements IBoardDatabaseProvider {
   public async loadAllBoards(): Promise<any[]> {
     const result = await db.query.board.findMany({
       with: {
-        cards: true
-      }
-    })
-    return result
+        cards: true,
+      },
+    });
+    return result;
   }
   public async createBoard(board: any): Promise<any> {
     const result = await db.insert(board).values({
       name: board.name,
-      cards: []
-    })
-    return result
+      cards: [],
+    });
+    return result;
+  }
+  public async deleteBoard(board: any, id: string): Promise<any> {
+    const result = await db
+      .update(board)
+      .set({ deleted: "1" })
+      .where(eq(board.id, id))
+      .execute();
+    return result;
   }
 }
