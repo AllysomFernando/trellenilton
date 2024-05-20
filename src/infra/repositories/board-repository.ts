cc1f628310696b00815770ed16e3c3e0ad33debf
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-05-20 15:36:14 -0300

    feat: adding createBoard at the board-repository

--- src/infra/repositories/board-repository.ts
+++ src/infra/repositories/board-repository.ts
@@ -3,19 +3,32 @@ import type { Board } from "../../domain/entities/board";
 import type { IBoardDatabaseProvider } from "../contracts/board-database-provider";
 
 export class BoardRepository implements IBoardRepository {
-  constructor(private readonly db: IBoardDatabaseProvider){}
+  constructor(private readonly db: IBoardDatabaseProvider) {}
 
   public async loadAllBoards(): Promise<Board[]> {
     const data = await this.db.loadAllBoards();
-    return data.map(board => ({
+    return data.map((board) => ({
       id: board.id,
       name: board.name,
       cards: board.cards.map((card: any) => ({
         id: card.id,
         name: card.name,
         description: card.description,
-        deadLine: new Date(card.deadLine)
-      }))
-    }))
+        deadLine: new Date(card.deadLine),
+      })),
+    }));
+  }
+  public async createBoard(board: Board): Promise<Board> {
+    const data = await this.db.createBoard(board);
+    return {
+      id: data.id,
+      name: data.name,
+      cards: data.cards.map((card: any) => ({
+        id: card.id,
+        name: card.name,
+        description: card.description,
+        deadLine: new Date(card.deadLine),
+      })),
+    };
   }
 }
