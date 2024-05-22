339b83318eb3bab9508b55699b2cd21c376793df
DiegoAATrevisan <augustodiego02@gmail.com>
2024-05-22 16:18:03 -0300

    Initialized Create Profile

---
+++ src/domain/features/create-profile.ts
@@ -0,0 +1,11 @@
+type Input = {}
+type Output = {}
+type CreateProfile = (input: Input)=> Promise<Output>
+type SetupCreateProfileProps = {}
+type Setup = ( props: SetupCreateProfileProps)=> CreateProfile
+
+export const setupCreateProfile: Setup = (props) => {
+    return async () => {
+        return {}
+    }
+}
