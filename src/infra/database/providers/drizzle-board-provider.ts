a50b627e531e91ca690f64f8161dc8a0ec1a3810
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-05-23 11:00:02 -0300

    feat: Update deleteBoard method in DrizzleBoardProvider

--- src/infra/database/providers/drizzle-board-provider.ts
+++ src/infra/database/providers/drizzle-board-provider.ts
@@ -1,29 +1,35 @@
 import { eq } from "drizzle-orm";
 import { db } from "../../../main/drizzle";
 import type { IBoardDatabaseProvider } from "../../contracts/board-database-provider";
-
+import { board } from "../models";
+import type { Board } from "../../../domain/entities/board";
 export class DrizzleBoardProvider implements IBoardDatabaseProvider {
-  public async loadAllBoards(): Promise<any[]> {
-    const result = await db.query.board.findMany({
-      with: {
-        cards: true,
-      },
-    });
-    return result;
-  }
-  public async createBoard(board: any): Promise<any> {
-    const result = await db.insert(board).values({
-      name: board.name,
-      cards: [],
-    });
-    return result;
-  }
-  public async deleteBoard(board: any, id: string): Promise<any> {
-    const result = await db
-      .update(board)
-      .set({ deleted: "1" })
-      .where(eq(board.id, id))
-      .execute();
-    return result;
-  }
+	public async loadAllBoards(): Promise<any[]> {
+		const result = await db.query.board.findMany({
+			with: {
+				cards: true,
+			},
+		});
+		return result;
+	}
+
+	public async createBoard(boardCreate: Board): Promise<any> {
+		const result = await db.insert(board).values({
+			id: boardCreate.id.toString(),
+			name: boardCreate.name,
+			deleted: "0",
+			createdAt: boardCreate.createdAt,
+		});
+		return result;
+	}
+
+	//TODO: verificar se eh a maneira correta passar esse board aqui
+	public async deleteBoard(board: any, id: string): Promise<any> {
+		const result = await db
+			.update(board)
+			.set({ deleted: false })
+			.where(eq(board.id, id))
+			.execute();
+		return result;
+	}
 }
