8093df7fa0a1962671b469c78d50613377e97318
Raul0P <raulparzianello@hotmail.com>
2024-05-23 20:37:37 -0300

    replace: createCard in diferent archive

--- src/infra/database/providers/drizzle-board-provider.ts
+++ src/infra/database/providers/drizzle-board-provider.ts
@@ -29,39 +29,4 @@ export class DrizzleBoardProvider implements IBoardDatabaseProvider {
     return result;
   }
 
-
-  public async createCard(dto: Card): Promise<Card> {
-    const priorityReference = await db.query.priority.findFirst({
-      where: eq(priority.id, dto.idPriority)
-    })
-    if (!priorityReference) {
-      throw new Error("Priority not found")
-    }
-    const categoryReference = await db.query.category.findFirst({
-      where: eq(category.id, dto.idCategory)
-    })
-    if (!categoryReference) {
-      throw new Error("Category not found")
-    }
-    const statusReference = await db.query.status.findFirst({
-      where: eq(status.id, dto.idStatus)
-    })
-    if (!statusReference) {
-      throw new Error("Status not found")
-    }
-    const result = await db.insert(card).values({
-
-      id: dto.id,
-      idPriority: priorityReference.id,
-      idCategory: categoryReference.id,
-      idStatus: statusReference.id,
-      title: dto.title,
-      description: dto.description,
-      createdAt: dto.createdAt,
-      updatedAt: dto.updatedAt,
-      endedAt: dto.endedAt,
-      deleted: 0,
-    })
-    return result;
-  }
 }
