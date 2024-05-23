ca3217fef5ed206573e8bf31481f5ca0ba444541
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-05-23 10:59:52 -0300

    feat: Update createBoard method signature to accept Board entity

--- src/infra/contracts/board-database-provider.ts
+++ src/infra/contracts/board-database-provider.ts
@@ -1,5 +1,7 @@
+import type { Board } from "../../domain/entities/board";
+
 export interface IBoardDatabaseProvider {
   loadAllBoards: () => Promise<any[]>;
-  createBoard: (board: any) => Promise<any>;
+  createBoard: (board: Board) => Promise<any>;
   deleteBoard: (board: any, id: string) => Promise<any>;
 }
