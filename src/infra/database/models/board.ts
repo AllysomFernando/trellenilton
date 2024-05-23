6adfcb1490f81c30fc94e0dc220bd5f038677665
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-05-23 10:59:57 -0300

    refactor: Update board model to use text type for the 'deleted' field

--- src/infra/database/models/board.ts
+++ src/infra/database/models/board.ts
@@ -2,14 +2,13 @@ import { relations } from "drizzle-orm";
 import { sqliteTable, text, int } from "drizzle-orm/sqlite-core";
 import { card } from "./card";
 
-
-export const board = sqliteTable('board', {
-  id: text('id').primaryKey(),
-  name: text("name").notNull(),
-  deleted: int("deleted").notNull(),
-  createdAt: text("createAt").notNull()
+export const board = sqliteTable("board", {
+	id: text("id").primaryKey(),
+	name: text("name").notNull(),
+	deleted: text("deleted").notNull(),
+	createdAt: text("createAt").notNull(),
 });
 
 export const boardRelations = relations(board, ({ many }) => ({
-  cards: many(card),
+	cards: many(card),
 }));
