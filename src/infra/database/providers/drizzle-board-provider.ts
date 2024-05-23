31d551046ffa8d8959449df4c0a653c335448647
Raul0P <raulparzianello@hotmail.com>
2024-05-23 19:59:43 -0300

    feat: createCard, with references in atb when contain relationship

--- src/infra/database/providers/drizzle-board-provider.ts
+++ src/infra/database/providers/drizzle-board-provider.ts
@@ -1,7 +1,8 @@
 import { eq } from "drizzle-orm";
 import { db } from "../../../main/drizzle";
 import type { IBoardDatabaseProvider } from "../../contracts/board-database-provider";
-import { card, priority } from "../models";
+import { card, category, priority, status } from "../models";
+import type { Card } from "../../../domain/entities/card";
 
 export class DrizzleBoardProvider implements IBoardDatabaseProvider {
   public async loadAllBoards(): Promise<any[]> {
@@ -29,16 +30,38 @@ export class DrizzleBoardProvider implements IBoardDatabaseProvider {
   }
 
 
-  public async createCard(_: any): Promise<any> {
+  public async createCard(dto: Card): Promise<Card> {
     const priorityReference = await db.query.priority.findFirst({
-      where: eq(priority.id, _.priorityid)
+      where: eq(priority.id, dto.idPriority)
     })
-    if(!priorityReference) {
+    if (!priorityReference) {
       throw new Error("Priority not found")
     }
-    await db.insert(card).values({
-      idPriority: priorityReference.id,
-      //all references in the db for the card need this changes
+    const categoryReference = await db.query.category.findFirst({
+      where: eq(category.id, dto.idCategory)
     })
+    if (!categoryReference) {
+      throw new Error("Category not found")
+    }
+    const statusReference = await db.query.status.findFirst({
+      where: eq(status.id, dto.idStatus)
+    })
+    if (!statusReference) {
+      throw new Error("Status not found")
+    }
+    const result = await db.insert(card).values({
+
+      id: dto.id,
+      idPriority: priorityReference.id,
+      idCategory: categoryReference.id,
+      idStatus: statusReference.id,
+      title: dto.title,
+      description: dto.description,
+      createdAt: dto.createdAt,
+      updatedAt: dto.updatedAt,
+      endedAt: dto.endedAt,
+      deleted: 0,
+    })
+    return result;
   }
 }
