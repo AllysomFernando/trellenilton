44a62a9a5c690ddc6ba199a50fa45ff3a109172c
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-05-23 11:00:06 -0300

    refactor: Update BoardRepository to include createdAt attribute in returned boards

--- src/infra/repositories/board-repository.ts
+++ src/infra/repositories/board-repository.ts
@@ -3,38 +3,40 @@ import type { Board } from "../../domain/entities/board";
 import type { IBoardDatabaseProvider } from "../contracts/board-database-provider";
 
 export class BoardRepository implements IBoardRepository {
-  constructor(private readonly db: IBoardDatabaseProvider) {}
+	constructor(private readonly db: IBoardDatabaseProvider) {}
 
-  public async loadAllBoards(): Promise<Board[]> {
-    const data = await this.db.loadAllBoards();
-    return data.map((board) => ({
-      id: board.id,
-      name: board.name,
-      cards: board.cards.map((card: any) => ({
-        id: card.id,
-        name: card.name,
-        description: card.description,
-        deadLine: new Date(card.deadLine),
-      })),
-      deleted: board.deleted,
-    }));
-  }
-  public async createBoard(board: Board): Promise<Board> {
-    const data = await this.db.createBoard(board);
-    return {
-      id: data.id,
-      name: data.name,
-      cards: data.cards.map((card: any) => ({
-        id: card.id,
-        name: card.name,
-        description: card.description,
-        deadLine: new Date(card.deadLine),
-      })),
-      deleted: board.deleted,
-    };
-  }
-  public async deleteBoard(board: Board, id: string): Promise<void> {
-    const data = await this.db.deleteBoard(board, id);
-    return data;
-  }
+	public async loadAllBoards(): Promise<Board[]> {
+		const data = await this.db.loadAllBoards();
+		return data.map((board) => ({
+			id: board.id,
+			name: board.name,
+			cards: board.cards.map((card: any) => ({
+				id: card.id,
+				name: card.name,
+				description: card.description,
+				deadLine: new Date(card.deadLine),
+			})),
+			deleted: board.deleted,
+			createdAt: board.createdAt,
+		}));
+	}
+	public async createBoard(board: Board): Promise<Board> {
+		const data = await this.db.createBoard(board);
+		return {
+			id: data.id,
+			name: data.name,
+			cards: data.cards.map((card: any) => ({
+				id: card.id,
+				name: card.name,
+				description: card.description,
+				deadLine: new Date(card.deadLine),
+			})),
+			deleted: board.deleted,
+			createdAt: board.createdAt,
+		};
+	}
+	public async deleteBoard(board: Board, id: string): Promise<void> {
+		const data = await this.db.deleteBoard(board, id);
+		return data;
+	}
 }
