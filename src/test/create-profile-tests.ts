96fe2f0e2bb3601436a85660a4e407c2d0795912
Diego Augusto Antonioli Trevisan <augustodiego02@gmail.com>
2024-06-05 20:40:48 -0300

    starting the tests.

---
+++ src/test/create-profile-tests.ts
@@ -0,0 +1,22 @@
+import { describe, test, expect } from 'bun:test'
+
+
+
+import { setupCreateProfile } from './create-profile'
+
+describe('create-profile', () => {
+    test('should create a profile', async () => {
+        const goodInput = {
+            name: "Diego",
+            function: "Admininastor",
+            image: "1",
+            deleted: 0
+        }
+        const response = await setupCreateProfile(goodInput)
+        expect(response).toEqual({
+            name: "Diego"
+        })
+    })
+
+    test("Should thrown an error when creating a profile")
+})
