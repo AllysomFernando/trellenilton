00443e9b309d1d8f3edf245980121945408afeb9
RaulParzianello <raulparzianello@hotmail.com>
2024-06-05 21:22:41 -0300

    feat: new function loadAllCard, in the repository

--- src/infra/repositories/card-repository.ts
+++ src/infra/repositories/card-repository.ts
@@ -5,6 +5,21 @@ import type { ICardDatabaseProvider } from "../contracts/card-database-provider"
 export class CardRepository implements ICardRepository {
     constructor(private readonly db: ICardDatabaseProvider) {}
 
+    public async loadAllCards(): Promise<Card[]> {
+        const data = await this.db.loadAllCards();
+        return data.map((card) => ({
+            id: card.id,
+            idPriority: card.idPriority,
+            idCategory: card.idCategory,
+            idStatus: card.idStatus,
+            title: card.title,
+            description: card.description,
+            createdAt: card.createdAt,
+            updatedAt: card.updatedAt,
+            endedAt: card.endedAt,
+            deleted: card.deleted,
+        }));
+    }
     public async createCard(card: Card): Promise<Card> {
         const data = await this.db.createCard(card);
         return {
