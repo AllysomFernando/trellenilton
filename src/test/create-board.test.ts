939ac7911edabfb3c4e5ef3d1b599019624cab1d
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-06 20:03:33 -0300

    chore: adding create-board-test

---
+++ src/test/create-board.test.ts
@@ -0,0 +1,46 @@
+import {describe, test, expect} from 'bun:test';
+
+type Card = {
+    name: string,
+    description: string,
+    deadLine: Date
+}
+type Board = {
+    cards?: Card[],
+    name: string
+    deleted: number
+}
+
+type CreateBoard = (dto: Board) => Pick<Board, "cards" | "name">
+const createBoard: CreateBoard = (dto) => {
+    if(dto.name.length < 5){
+        throw new Error("The name must be at least with 5 characters ")
+    }
+    return {
+        name: dto.name
+    }
+}
+
+describe("CreateBoard", () => {
+    test("Should create a board", ()=> {
+        const goodPath = {
+            name: "DiegoBoard",
+            deleted: 0
+        }
+        const response = createBoard(goodPath)
+        expect(response).toEqual({
+            name: "DiegoBoard"
+        })
+    })
+    test("should throws if pass a name with less than 5 characters", ()=> {
+        const badInput = {
+            name: "DiegoBoard",
+            deleted: 0
+        }
+        try{
+            createBoard(badInput)
+        }catch(error){
+            expect(error).toEqual(new Error("Name must be at least 5 characters"));
+        }
+    })
+})
