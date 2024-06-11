d6d53129fd4cb56ed3001d1bed8a7244b308bdf4
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-11 19:28:55 -0300

    fix: removing atribute on deleteBoard

--- src/infra/contracts/board-database-provider.ts
+++ src/infra/contracts/board-database-provider.ts
@@ -1,6 +1,6 @@
 export interface IBoardDatabaseProvider {
   loadAllBoards: () => Promise<any[]>;
   createBoard: (board: any) => Promise<any>;
-  deleteBoard: (board: any, id: string) => Promise<any>;
+  deleteBoard: (id: string) => Promise<any>;
   updateBoard: (board: any, id: string) => Promise<any>;
 }
