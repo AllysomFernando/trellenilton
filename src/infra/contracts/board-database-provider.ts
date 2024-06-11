3f3ed6cb1afc09303ebe7aeaa8fb35cbe18218c3
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-11 17:33:47 -0300

    chore: adding updateBoard at board-database-provider

--- src/infra/contracts/board-database-provider.ts
+++ src/infra/contracts/board-database-provider.ts
@@ -2,4 +2,5 @@ export interface IBoardDatabaseProvider {
   loadAllBoards: () => Promise<any[]>;
   createBoard: (board: any) => Promise<any>;
   deleteBoard: (board: any, id: string) => Promise<any>;
+  updateBoard: (board: any, id: string) => Promise<any>;
 }
