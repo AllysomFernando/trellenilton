943d120276fef672481fa028f2b6bbb7f20bc5d3
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-11 19:30:03 -0300

    fix: fixing imports and atributes

--- src/infra/database/providers/drizzle-board-provider.ts
+++ src/infra/database/providers/drizzle-board-provider.ts
@@ -1,6 +1,7 @@
 import { eq } from "drizzle-orm";
 import { db } from "../../../main/drizzle";
 import type { IBoardDatabaseProvider } from "../../contracts/board-database-provider";
+import { board } from "../models";
 
 export class DrizzleBoardProvider implements IBoardDatabaseProvider {
   public async loadAllBoards(): Promise<any[]> {
@@ -14,14 +15,16 @@ export class DrizzleBoardProvider implements IBoardDatabaseProvider {
   public async createBoard(board: any): Promise<any> {
     const result = await db.insert(board).values({
       name: board.name,
-      cards: [],
+      id: board.id,
+      deleted: board.deleted,
+      createdAt: new Date().toISOString()
     });
     return result;
   }
-  public async deleteBoard(board: any, id: string): Promise<any> {
+  public async deleteBoard(id: string): Promise<void> {
     const result = await db
       .update(board)
-      .set({ deleted: "1" })
+      .set({ deleted: 1 })
       .where(eq(board.id, id))
       .execute();
     return result;
