20b723e08d9319fe5afdf50d019618e8043aaff3
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-12 18:27:14 -0300

    chore: adding a new type

--- src/presentation/next-frontend/src/types/board.ts
+++ src/presentation/next-frontend/src/types/board.ts
@@ -1,3 +1,9 @@
+export interface Board {
+    id: string;
+    name: string;
+    deleted: boolean;
+}
+
 export interface UpdateBoardData {
     name?: string;
     cards?: Array<{id: string, idPrioridade: string, idCategoria: string, idStatus: string, title: string, description: string, createdAt: string, updatedAt: string, endedAt: string, deleted: boolean }>
