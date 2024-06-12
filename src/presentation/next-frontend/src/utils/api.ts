f1b6321ebfc55648c81243d8c8aa7abdc62cc0c6
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-12 16:54:39 -0300

    chore: adding utils api

---
+++ src/presentation/next-frontend/src/utils/api.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.response.use(
+    response => response,
+    error =>{
+        if(error.response.status) {
+            console.log(error.response.data);
+        }
+        return Promise.reject(error);
+    }
+)
+
+export const fetcher = async (url: string) => {
+    try {
+        const response = await api.get(url);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const poster = async (url: string, data: any) => {
+    try {
+        const response = await api.post(url, data);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default api;
