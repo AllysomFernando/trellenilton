f26692dcf51a8b2e5a7607b5dbac2a603f619b55
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-12 14:45:00 -0300

    chore: just testing the connection

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+require('dotenv').config();
+
+const {neon} = require('@neondatabase/serverless');
+
+const {PGHOST, PGDATABASE, PGUSER, PGPASSWORD} = process.env;
+
+const sql = neon(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`);
+
+async function getPgVersion(){
+    const result = await sql`SELECT version()`;
+    console.log(result[0]);
+}
+
+getPgVersion(); 
