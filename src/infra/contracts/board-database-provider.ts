666f16411033a834def756fdadf4ba4a7c147cf3
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-12 14:46:28 -0300

    fix: removing the Board type from deletedBoard

--- src/infra/contracts/board-database-provider.ts
+++ src/infra/contracts/board-database-provider.ts
@@ -1,6 +1,6 @@
 export interface IBoardDatabaseProvider {
   loadAllBoards: () => Promise<any[]>;
   createBoard: (board: any) => Promise<any>;
-  deleteBoard: (id: string) => Promise<any>;
+  deleteBoard: (id: string) => Promise<void>;
   updateBoard: (board: any, id: string) => Promise<any>;
 }
