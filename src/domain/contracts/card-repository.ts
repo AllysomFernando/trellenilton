ea2c0be9f1be7e566b27fabf2f957bfae6ec9970
RaulParzianello <raulparzianello@hotmail.com>
2024-06-12 21:55:40 -0300

    feat: new feature in the ICardRepo findCardByTitle

--- src/domain/contracts/card-repository.ts
+++ src/domain/contracts/card-repository.ts
@@ -5,4 +5,5 @@ export interface ICardRepository {
     createCard: (card: Card) => Promise<Card>;
     loadAllCards: () => Promise<Card[]>;
     deleteCard: (card: Card, id: string) => Promise<void>;
+    findCardByTitle: (title: string) => Promise<Card|null>;
 }
