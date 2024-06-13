4c34860d4d72ef1dd808076cfe7c9440f051b1c0
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-13 09:25:06 -0300

    fix: changing type

--- src/infra/contracts/board-database-provider.ts
+++ src/infra/contracts/board-database-provider.ts
@@ -2,7 +2,7 @@ import type { Board } from "../../domain/entities/board";
 
 export interface IBoardDatabaseProvider {
 	loadAllBoards: () => Promise<any[]>;
-	createBoard: (board: any) => Promise<any>;
+	createBoard: (board: Board) => Promise<any>;
 	deleteBoard: (id: string) => Promise<void>;
-	updateBoard: (board: any, id: string) => Promise<any>;
+	updateBoard: (board: Board, id: string) => Promise<any>;
 }
