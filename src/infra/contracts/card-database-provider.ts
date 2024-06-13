57049e3959691fc20fa53bf3e100ee64697dee61
RaulParzianello <raulparzianello@hotmail.com>
2024-06-12 22:17:16 -0300

    new feature in the databaseprovider

--- src/infra/contracts/card-database-provider.ts
+++ src/infra/contracts/card-database-provider.ts
@@ -1,7 +1,10 @@
+import type { Card } from "../../domain/entities/card";
+
 export interface ICardDatabaseProvider {
 
     loadAllCards: () => Promise<any[]>;
     createCard: (card: any) => Promise<any>;
-    deleteCard: (card: any, id: string) => Promise<any>;
+    deleteCard: (id: string) => Promise<any>;
+    findCardByTitle: (title: string) => Promise<any>;
 }
   
