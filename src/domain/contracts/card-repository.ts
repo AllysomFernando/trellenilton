776e676808071e711940fce958fecf6934399af7
RaulParzianello <raulparzianello@hotmail.com>
2024-06-12 22:11:38 -0300

    fix: remove null in hte promise of the feature findcard...

--- src/domain/contracts/card-repository.ts
+++ src/domain/contracts/card-repository.ts
@@ -5,5 +5,5 @@ export interface ICardRepository {
     createCard: (card: Card) => Promise<Card>;
     loadAllCards: () => Promise<Card[]>;
     deleteCard: (id: string) => Promise<void>;
-    findCardByTitle: (title: string) => Promise<Card|null>;
+    findCardByTitle: (title: string) => Promise<Card>;
 }
