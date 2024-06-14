058e5b8647b28faa11d96b3c5da911310b78d9cf
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-13 22:04:44 -0300

    chore: adding try catch block

--- src/presentation/next-frontend/src/services/boardServices.ts
+++ src/presentation/next-frontend/src/services/boardServices.ts
@@ -1,27 +1,39 @@
-import api from '../utils/api';
-import { Board, UpdateBoardData } from '../types/board';
+import api, { fetcher, poster } from "../utils/api";
+import { Board, UpdateBoardData } from "../types/board";
 
 export const fetchBoards = async () => {
-  const response = await api.get('/boards');
-  return response.data;
+	try {
+		const response = await fetcher("/boards");
+		if (response.data) {
+			return response.data;
+		} else {
+			return [];
+		}
+	} catch (error) {
+		console.log(error);
+	}
 };
-
-export const createBoard = async (data: { name: string }): Promise<Board> => {
-  const response = await api.post<Board>('/boards', data);
-  return response.data;
+export const createBoard = async (data: { name: string }): Promise<Board | undefined> => {
+	try {
+	   const response = await poster('/create-boards', data);
+	   return response.data;
+	} catch (error) {
+	   console.log(error);
+	}
+	return undefined;
 };
 
 export const fetchBoardById = async (id: string) => {
-  const response = await api.get(`/boards/${id}`);
-  return response.data;
+	const response = await fetcher(`/boards/${id}`);
+	return response.data;
 };
 
 export const updateBoard = async (id: string, data: UpdateBoardData) => {
-  const response = await api.put(`/boards/${id}`, data);
-  return response.data;
+	const response = await poster(`/boards/${id}`, data);
+	return response.data;
 };
 
 export const deleteBoard = async (id: string) => {
-  const response = await api.delete(`/boards/${id}`);
-  return response.data;
+	const response = await api.delete(`/boards/${id}`);
+	return response.data;
 };
