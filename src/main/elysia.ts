f634040d41968b76974216469b45749c138c92a5
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-13 21:54:24 -0300

    chore: add cors

--- src/main/elysia.ts
+++ src/main/elysia.ts
@@ -1,24 +1,27 @@
-import { setupCreateBoards } from 'domain/features/create-boards'
-import Elysia, { t } from 'elysia'
-import { DrizzleBoardProvider } from 'infra/database/providers/drizzle-board-provider'
-import { BoardRepository } from 'infra/repositories/board-repository'
-
+import { setupCreateBoards } from "domain/features/create-boards";
+import Elysia, { t } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { DrizzleBoardProvider } from "infra/database/providers/drizzle-board-provider";
+import { BoardRepository } from "infra/repositories/board-repository";
 
 new Elysia()
-  .get("/health", () => "it's healthy")
-  .decorate("service", setupCreateBoards({
-    repository: new BoardRepository(
-      new DrizzleBoardProvider()
-    )
-  }))
-  .post('/api/create-boards', ({
-    body: { name },
-    service
-  }) => {
-    return service({name});
-  }, {
-    body: t.Object({
-      name: t.String(),
-    })
-  })
-  .listen(3000);
+	.use(cors())
+	.get("/health", () => "it's healthy")
+	.decorate(
+		"service",
+		setupCreateBoards({
+			repository: new BoardRepository(new DrizzleBoardProvider()),
+		})
+	)
+	.post(
+		"/api/create-boards",
+		({ body: { name }, service }) => {
+			return service({ name });
+		},
+		{
+			body: t.Object({
+				name: t.String(),
+			}),
+		}
+	)
+	.listen(3000);
