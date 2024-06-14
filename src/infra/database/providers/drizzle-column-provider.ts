a5d4438e015bca42c92eb699b47ea7da2ce1e441
DiegoAATrevisan <augustodiego02@gmail.com>
2024-06-14 20:44:19 -0300

    Implemented the function that will load all columns from the database.

--- src/infra/database/providers/drizzle-column-provider.ts
+++ src/infra/database/providers/drizzle-column-provider.ts
@@ -1,6 +1,5 @@
 import { db } from "../../../main/drizzle";
 import type { IColumnDatabseProvider } from "../../contracts/column-database-provider";
-import { card } from "../models";
 
 export class DrizzleColumnProvider implements IColumnDatabseProvider {
     public async createColumn(column: any): Promise<any> {
@@ -10,5 +9,16 @@ export class DrizzleColumnProvider implements IColumnDatabseProvider {
             }
         )
         return result;
+    }
+    public async loadAllColumns(): Promise<any[]> {
+        try {
+            const result = await db.query.column.findMany()
+            return result;
+        } catch (error) {
+            throw new Error("Could not load all columns", {
+                cause: "load-all-columns"
+            })
+        }
+        return []
     }
 }
