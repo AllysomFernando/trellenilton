cc6f80b31f97633fda4975a168ee974caf7d2089
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-17 14:34:02 -0300

    chore: adding new atributes to column

--- src/domain/entities/column.ts
+++ src/domain/entities/column.ts
@@ -2,7 +2,9 @@ import type { Card } from "./card";
 
 export type Column = {
     id: string;
+    name: string;
     ordernation: string;
     cards: Card[];
     deleted: boolean;
+    description: string;
 }
