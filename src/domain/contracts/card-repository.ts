1872bdc9ca71d6b5b4e690c246282b72765fc9ae
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-17 14:40:39 -0300

    chore: lint

--- src/domain/contracts/card-repository.ts
+++ src/domain/contracts/card-repository.ts
@@ -1,7 +1,6 @@
 import type { Card } from "../entities/card";
 
 export interface ICardRepository {
-
     createCard: (card: Card) => Promise<Card>;
     loadAllCards: () => Promise<Card[]>;
     deleteCard: (card: Card, id: string) => Promise<void>;
