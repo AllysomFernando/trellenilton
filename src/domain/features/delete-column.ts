f16e26ad9b5cc69cf305648dafc1379617080cbf
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-17 14:29:43 -0300

    chore: adding DTO to deleteColumn

---
+++ src/domain/features/delete-column.ts
@@ -0,0 +1,37 @@
+import type { IColumnRepository } from "domain/contracts/column-repository";
+import type { Column } from "../entities/column";
+
+type Input = {
+	column: Pick<Column, "deleted">;
+	id: string;
+};
+type Output = {
+	statusCode: number;
+	message: string;
+};
+type DeleteColumn = (input: Input) => Promise<Output>;
+type SetupDeleteColumn = {
+	repository: IColumnRepository;
+};
+type Setup = (props: SetupDeleteColumn) => DeleteColumn;
+
+export const SetupDeleteColumn: Setup =
+	({ repository }) =>
+	async ({ column, id }) => {
+		try {
+			if (column.deleted === false) {
+				throw new Error("Column must be marked as deleted before deletion", {
+					cause: "column-not-deleted",
+				});
+			}
+			await repository.deleteColumn(id);
+			return {
+				statusCode: 200,
+				message: "Column deleted successfully",
+			};
+		} catch (error) {
+			throw new Error("Could not delete the column", {
+				cause: "delete-column",
+			});
+		}
+	};
