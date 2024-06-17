90566fce9ff087b9627560a29f98afa82db57742
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-17 14:33:10 -0300

    chore: adding new features to column-reopository

--- src/domain/contracts/column-repository.ts
+++ src/domain/contracts/column-repository.ts
@@ -1,6 +1,8 @@
 import type { Column } from "../entities/column";
 
 export interface IColumnRepository {
-    createColumn: (deleted: boolean) => Promise<Column>
+    createColumn: (name: string, deleted: boolean, description: string) => Promise<Column>
     loadAllColumns: () => Promise<Column[]>;
+    deleteColumn: (id: string) => Promise<void>;
+    updateColumn: (id: string, name: string) => Promise<Column>;
 }
