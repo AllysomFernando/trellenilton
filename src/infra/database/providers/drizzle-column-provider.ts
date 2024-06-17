67f7c6a035fb92053d65cba80c25f9a53f395d84
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-17 14:35:32 -0300

    chore: adding new features

--- src/infra/database/providers/drizzle-column-provider.ts
+++ src/infra/database/providers/drizzle-column-provider.ts
@@ -1,24 +1,51 @@
 import { db } from "../../../main/drizzle";
+import { eq } from "drizzle-orm";
 import type { IColumnDatabseProvider } from "../../contracts/column-database-provider";
-
+import { column } from "../models";
 export class DrizzleColumnProvider implements IColumnDatabseProvider {
-    public async createColumn(column: any): Promise<any> {
-        const result = await db.insert(column).values(
-            {
-                deleted: column.deleted,
-            }
-        )
-        return result;
-    }
-    public async loadAllColumns(): Promise<any[]> {
-        try {
-            const result = await db.query.column.findMany()
-            return result;
-        } catch (error) {
-            throw new Error("Could not load all columns", {
-                cause: "load-all-columns"
-            })
-        }
-        return []
-    }
+	public async createColumn(
+		name: string,
+		deleted: boolean,
+		description: string
+	): Promise<any> {
+		const result = await db.insert(column).values({
+			name: name,
+			deleted: deleted,
+			description: description,
+		});
+		return result;
+	}
+	public async loadAllColumns(): Promise<any[]> {
+		try {
+			const result = await db.query.column.findMany();
+			return result;
+		} catch (error) {
+			throw new Error("Could not load all columns", {
+				cause: "load-all-columns",
+			});
+		}
+	}
+	public async deleteColumn(id: string): Promise<void> {
+		try {
+			await db
+				.update(column)
+				.set({ deleted: true })
+				.where(eq(column.id, id))
+				.execute();
+		} catch (error) {
+			console.error(error);
+		}
+	}
+	public async updateColumn(id: string, name: string): Promise<any> {
+		try {
+			const result = await db
+				.update(column)
+				.set({ name: name })
+				.where(eq(column.id, id))
+				.execute();
+			return result;
+		} catch (error) {
+			console.error(error);
+		}
+	}
 }
