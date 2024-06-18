d705d6d181baa9983e590473cb091b980c2de0b5
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-18 16:03:38 -0300

    feat: Atualizar página inicial para exibir lista de quadros

--- src/presentation/frontend/src/pages/index.tsx
+++ src/presentation/frontend/src/pages/index.tsx
@@ -1,19 +1,11 @@
-import React from "react";
-import BoardForm from "../components/Board/BoardForm";
 import BoardList from "@/components/Board/BoardList";
-import { authorQuoteMap, quotes } from "@/utils/data";
-import { Board } from "@/components/Board/Boards";
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
-const Home = () => {
+
+const BoardsPage = () => {
 	return (
-		<div className={inter.className}>
-			<h1>My Boards</h1>
-			<BoardForm onSubmit={(e) => console.log("submited", e)} />
+		<div className="container mx-auto py-8">
 			<BoardList />
-			<Board initial={authorQuoteMap} />
 		</div>
 	);
 };
 
-export default Home;
+export default BoardsPage;
