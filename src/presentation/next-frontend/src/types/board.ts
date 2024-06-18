738a74be2e7b1af14d5a0ec9bd27ac4e9713c444
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-18 11:11:45 -0300

    feat: Add types for board, author, quote, dragging, quote map, and task

--- src/presentation/next-frontend/src/types/board.ts
+++ src/presentation/next-frontend/src/types/board.ts
@@ -1,3 +1,7 @@
+import type { DraggableId, DraggableLocation } from "@hello-pangea/dnd";
+
+export type Id = string;
+
 export interface Board {
 	id: string;
 	name: string;
@@ -10,4 +14,35 @@ export interface UpdateBoardData {
 
 export interface DeleteBoardData {
 	deleted: boolean;
+}
+export interface AuthorColors {
+	soft: string;
+	hard: string;
+}
+
+export interface Author {
+	id: Id;
+	name: string;
+	url: string;
+	colors: AuthorColors;
+}
+
+export interface Quote {
+	id: Id;
+	content: string;
+	author: Author;
+}
+
+export interface Dragging {
+	id: DraggableId;
+	location: DraggableLocation;
+}
+
+export interface QuoteMap {
+	[key: string]: Quote[];
+}
+
+export interface Task {
+	id: Id;
+	content: string;
 }
