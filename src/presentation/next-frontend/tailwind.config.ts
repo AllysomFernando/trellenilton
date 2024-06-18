0945f7076845009004bc3b6385661036e12b0e57
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-18 11:04:21 -0300

    chore: Update tailwind.config.ts with new colors, animations, and container padding

--- src/presentation/next-frontend/tailwind.config.ts
+++ src/presentation/next-frontend/tailwind.config.ts
@@ -12,9 +12,36 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        site: 'url("/site-bg.svg")',
+      },
+      colors: {
+        primary: '#131424',
+        secondary: '#393A47',
+        accent: '#F13024',
+      },
+      animation: {
+        'spin-slow': 'spin 6s linear infinite',
+      },
+      fontFamily: {
+        poppins: [`var(--font-poppins)`, 'sans-serif'],
+        sora: [`var(--font-sora)`, 'sans-serif'],
       },
     },
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    }
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dark"],
+  },
 };
 export default config;
