e1608cbd9560fc28013805053e3e4bbf045647c8
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-18 16:03:22 -0300

    feat: Add onClose functionality to BoardForm component

--- src/presentation/frontend/src/components/Board/BoardForm.tsx
+++ src/presentation/frontend/src/components/Board/BoardForm.tsx
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { createBoard, updateBoard } from "../../services/boardServices";
 
 interface BoardFormProps {
 	boardId?: string;
 	initialData?: { name: string };
 	onSubmit: (updatedData: { name: string; deleted: boolean }) => void;
+	onClose: () => void;
 }
 
-const BoardForm = ({ boardId, initialData, onSubmit }: BoardFormProps) => {
+const BoardForm = ({
+	boardId,
+	initialData,
+	onSubmit,
+	onClose,
+}: BoardFormProps) => {
 	const [name, setName] = useState(initialData ? initialData.name : "");
+
+	useEffect(() => {
+		if (initialData) {
+			setName(initialData.name);
+		}
+	}, [initialData]);
 
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
@@ -23,9 +35,7 @@ const BoardForm = ({ boardId, initialData, onSubmit }: BoardFormProps) => {
 
 		try {
 			if (boardId) {
-				const updatedBoard = await updateBoard(boardId, {
-					name,
-				});
+				const updatedBoard = await updateBoard(boardId, { name });
 				if (onSubmit) {
 					onSubmit(updatedBoard || { name: "", deleted: false });
 				}
@@ -35,6 +45,7 @@ const BoardForm = ({ boardId, initialData, onSubmit }: BoardFormProps) => {
 					onSubmit(newBoard || { name: "", deleted: false });
 				}
 			}
+			onClose();
 			window.location.reload();
 		} catch (error) {
 			console.error("Failed to save board:", error);
@@ -44,15 +55,30 @@ const BoardForm = ({ boardId, initialData, onSubmit }: BoardFormProps) => {
 
 	return (
 		<form onSubmit={handleSubmit}>
-			<label>
+			<label className="block text-gray-700 text-sm font-bold mb-2">
 				Name:
 				<input
 					type="text"
 					value={name}
 					onChange={(e) => setName(e.target.value)}
+					className="mt-1 p-2 border rounded w-full"
 				/>
 			</label>
-			<button type="submit">{boardId ? "Update" : "Create"} Board</button>
+			<div className="flex justify-end space-x-2">
+				<button
+					type="submit"
+					className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+				>
+					{boardId ? "Update" : "Create"} Board
+				</button>
+				<button
+					type="button"
+					onClick={onClose}
+					className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-700"
+				>
+					Cancel
+				</button>
+			</div>
 		</form>
 	);
 };
