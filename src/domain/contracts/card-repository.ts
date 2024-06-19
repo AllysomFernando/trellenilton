3a1eb39e1d24d873a91f9feb0895aa45bded15ee
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-19 18:35:02 -0300

    Atualizar contrato do repositório de cartões

--- src/domain/contracts/card-repository.ts
+++ src/domain/contracts/card-repository.ts
@@ -1,12 +1,16 @@
 import type { Card } from "../entities/card";
 
 export interface ICardRepository {
-    createCard: (card: Card) => Promise<Card>;
-    loadAllCards: () => Promise<Card[]>;
-    displayCard: (id: string) => Promise<Card>;
-    deleteCard: (id: string) => Promise<void>;
-    findCardByTitle: (title: string) => Promise<Card>;
-    renameCard: (card: Card, id: string) => Promise<Card>;
-    insertCardDescription: (card: Card, id: string, description: string) => Promise<Card>;
-    insertCardDeadline: (card: Card, id: string, endedAt: string, createdAt: string) => Promise<Card>;
+	createCard: (card: Card) => Promise<Card>;
+	loadAllCards: () => Promise<Card[]>;
+	displayCard: (id: string) => Promise<Card>;
+	deleteCard: (id: string) => Promise<void>;
+	findCardByTitle: (title: string) => Promise<Card>;
+	renameCard: (id: string, title: string) => Promise<Card>;
+	insertCardDescription: (id: string, description: string) => Promise<Card>;
+	insertCardDeadline: (
+		id: string,
+		endedAt: string,
+		createdAt: string
+	) => Promise<Card>;
 }
