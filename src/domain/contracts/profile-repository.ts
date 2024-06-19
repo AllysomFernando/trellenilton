815e8864e6fc38ba34318dc0634658f279911301
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-19 18:36:38 -0300

    Atualizar contrato do repositório de perfis para adicionar método de renomear perfil

--- src/domain/contracts/profile-repository.ts
+++ src/domain/contracts/profile-repository.ts
@@ -1,8 +1,12 @@
 import type { Profile } from "../entities/profile";
 
 export interface IProfileRepository {
-    createProfile: (name: string, deleted: boolean, funcao: string) => Promise<Profile>
-    loadAllProfile: () => Promise<Profile[]>
-    deleteProfile: (id: string) => Promise<void>
-    renameProfile: (profile: Profile, id: string) => Promise<Profile>
+	createProfile: (
+		name: string,
+		deleted: boolean,
+		funcao: string
+	) => Promise<Profile>;
+	loadAllProfile: () => Promise<Profile[]>;
+	deleteProfile: (id: string) => Promise<void>;
+	renameProfile: (id: string, name: string) => Promise<Profile>;
 }
