88c39a870894b76fa93bd4fadac8df0b9e4ccfeb
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-19 18:34:59 -0300

    feat: Refactor createCard function for better readability and error handling

--- src/domain/features/create-card.ts
+++ src/domain/features/create-card.ts
@@ -2,54 +2,66 @@ import type { Card } from "../entities/card";
 import type { ICardRepository } from "../contracts/card-repository";
 
 type Input = {
-    id: string;
-    idPriority: string;
-    idCategory: string;
-    idStatus: string;
-    title: string;
-    description?: string;
-    createdAt: string;
-    updatedAt?: string;
-    endedAt?: string;
-    deleted: boolean;
+	id: string;
+	idPriority: string;
+	idCategory: string;
+	idStatus: string;
+	title: string;
+	description?: string;
+	createdAt: string;
+	updatedAt?: string;
+	endedAt?: string;
+	deleted: boolean;
 };
 type Output = Card;
 type CreateCard = (input: Input) => Promise<Output>;
 type SetupCreateCard = {
-    repository: ICardRepository;
+	repository: ICardRepository;
 };
 type Setup = (props: SetupCreateCard) => CreateCard;
 
 export const SetupCreateCard: Setup =
-    ({ repository }) =>
-        async ({id, idPriority, idCategory, idStatus, title, description, createdAt, updatedAt, endedAt, deleted}) => {
-            if(title.length < 3) throw new Error("Title must be at least 3 characters long", {
-                cause: "invalid-card-title",
-            });
-            
-            const existingCard = await repository.findCardByTitle(title);
-            if (existingCard) {
-                throw new Error("Another card with the same title already exists", {
-                    cause: "duplicate-card-title",
-                });
-            }
+	({ repository }) =>
+	async ({
+		id,
+		idPriority,
+		idCategory,
+		idStatus,
+		title,
+		description,
+		createdAt,
+		updatedAt,
+		endedAt,
+		deleted,
+	}) => {
+		if (title.length < 3)
+			throw new Error("Title must be at least 3 characters long", {
+				cause: "invalid-card-title",
+			});
 
-            try {
-                return await repository.createCard({
-                    id: id,
-                    idPriority: idPriority, 
-                    idCategory: idCategory,
-                    idStatus: idStatus,
-                    title: title,
-                    description: description,
-                    createdAt: createdAt,
-                    updatedAt: updatedAt,
-                    endedAt: endedAt,
-                    deleted: deleted,
-                });
-            } catch (error) {
-                throw new Error("Could not create card", {
-                    cause: "create-card",
-                });
-            }
-        };
+		const existingCard = await repository.findCardByTitle(title);
+		if (existingCard) {
+			throw new Error("Another card with the same title already exists", {
+				cause: "duplicate-card-title",
+			});
+		}
+
+		try {
+			return await repository.createCard({
+				id,
+				idPriority,
+				idCategory,
+				idStatus,
+				title,
+				description,
+				createdAt,
+				updatedAt,
+				endedAt,
+				deleted,
+			});
+		} catch (error) {
+			throw new Error("Could not create card", {
+				cause: "create-card",
+			});
+		}
+	};
