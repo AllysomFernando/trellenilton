65ac2521323ba4569e5a4d6bb510c0e8dca552a0
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-19 21:58:58 -0300

    Adiciona campo "deleted" na tabela "priority"

--- src/infra/database/models/priority.ts
+++ src/infra/database/models/priority.ts
@@ -1,10 +1,11 @@
 import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { boolean, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const priority = pgTable("priority", {
 	id: uuid("id").defaultRandom().primaryKey(),
 	name: varchar("name").notNull(),
 	level: varchar("level").notNull(),
+	deleted: boolean("deleted").notNull().default(false),
 });
 
 export type Priority = InferSelectModel<typeof priority>;
