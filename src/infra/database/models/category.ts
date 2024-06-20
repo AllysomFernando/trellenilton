0e2db0fa5335f3c74a033261baec9c02710ae6bc
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-20 09:33:05 -0300

    feat: Adiciona campo "deleted" na tabela "category"

--- src/infra/database/models/category.ts
+++ src/infra/database/models/category.ts
@@ -1,10 +1,11 @@
 import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { boolean, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const category = pgTable("category", {
 	id: uuid("id").defaultRandom().primaryKey(),
 	name: varchar("name").notNull(),
 	description: varchar("description").notNull(),
+	deleted: boolean("deleted").default(false),
 });
 
 export type Category = InferSelectModel<typeof category>;
