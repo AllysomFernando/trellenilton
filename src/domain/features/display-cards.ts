88f85be9edeae552e1634531f2e8c5a2a204073f
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-20 16:58:26 -0300

    refactor: Update setupDisplayCards function to handle empty card array

--- src/domain/features/display-cards.ts
+++ src/domain/features/display-cards.ts
@@ -1,26 +1,28 @@
-import type { ICardRepository } from "../contracts/card-repository"
-import type { Card } from "../entities/card"
+import type { ICardRepository } from "../contracts/card-repository";
+import type { Card } from "../entities/card";
 
-type Input = {}
-type Output = Card[]
-type DisplayCards = (input: Input) => Promise<Output>
+type Input = {};
+type Output = Card[];
+type DisplayCards = (input: Input) => Promise<Output>;
 type SetupDisplayCardsProps = {
-    repository: ICardRepository
-}
-type Setup = (props: SetupDisplayCardsProps) => DisplayCards
+	repository: ICardRepository;
+};
+type Setup = (props: SetupDisplayCardsProps) => DisplayCards;
 
-export const setupDisplayCards: Setup = ({
-    repository
-}) => async input => {
-    try {
-        const cards = await repository.loadAllCards()
-        if (cards.length === 0) throw new Error ("No cards found", {
-            cause: "no-cards"
-        })
-        return cards
-    } catch (error) {
-        throw new Error("Could not load all cards", {
-            cause: "display-cards"
-        })
-    }
-}
+export const setupDisplayCards: Setup =
+	({ repository }) =>
+	async (input) => {
+		try {
+			const cards = await repository.loadAllCards();
+			if (cards.length === 0)
+				throw new Error("No cards found", {
+					cause: "no-cards",
+				});
+			return cards;
+		} catch (error) {
+			console.log(error);
+			throw new Error("Could not load all cards", {
+				cause: "display-cards",
+			});
+		}
+	};
