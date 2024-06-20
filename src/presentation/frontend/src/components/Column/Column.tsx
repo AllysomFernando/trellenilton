0f2d7cafa8b9ed4dd39fc1327f54b3a9b97cb307
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-20 11:54:07 -0300

    feat: Refatorar recurso de criação de cartão para incluir valores padrão para descrição, updatedAt e endedAt

--- src/presentation/frontend/src/components/Column/Column.tsx
+++ src/presentation/frontend/src/components/Column/Column.tsx
@@ -10,10 +10,10 @@ import type {
 import { grid, borderRadius } from "@/utils/constants";
 import Title from "@/components/primatives/title";
 import AddCardForm from "@/components/Card/CardForm";
-import type { Quote } from "@/types/board";
+import type { Quote, NewCard } from "@/types/board";
 import { Menu } from "@headlessui/react";
 import { deleteColumn } from "@/services/columnService";
-import CreateCardModal from "@/components/modal/NewCardModal";
+import NewCardModal from "@/components/modal/NewBoardModal";
 
 const Container = styled.div`
 	margin: ${grid}px;
@@ -60,13 +60,13 @@ interface Props {
 	index: number;
 	isScrollable?: boolean;
 	isCombineEnabled?: boolean;
+	priorities: string[];
+	categories: string[];
+	statuses: string[];
 	useClone?: boolean;
 	onAddCard: (columnId: string, card: Quote) => void;
 	onEdit: () => void;
 	onDelete: () => void;
-	priorities: string[];
-	categories: string[];
-	statuses: string[];
 }
 
 const Column: React.FC<Props> = ({
@@ -80,11 +80,18 @@ const Column: React.FC<Props> = ({
 	onAddCard,
 	onEdit,
 	onDelete,
-	priorities,
-	categories,
-	statuses,
 }) => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const handleAddCard = (newCard: NewCard) => {
+		const newQuote: Quote = {
+			id: `quote-${Date.now()}`,
+			content: newCard.title,
+			deleted: false,
+		};
+		onAddCard(columnId, newQuote);
+		setIsModalOpen(false);
+	};
 
 	const handleDelete = async () => {
 		try {
@@ -98,109 +105,114 @@ const Column: React.FC<Props> = ({
 	const quotesList = Array.isArray(quotes) ? quotes : [];
 
 	return (
-		<Draggable draggableId={columnId} index={index}>
-			{(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
-				<Container ref={provided.innerRef} {...provided.draggableProps}>
-					<Header isDragging={snapshot.isDragging}>
-						<Title
-							{...provided.dragHandleProps}
-							aria-label={`${title} quote list`}
-						>
-							{title}
-						</Title>
-						<Menu as="div" className="absolute top-2 right-2">
-							<Menu.Button className="text-gray-500 hover:text-gray-700">
-								•••
-							</Menu.Button>
-							<Menu.Items className="absolute right-0 w-48 py-1 mt-2 bg-white border border-gray-200 rounded-md shadow-lg">
-								<Menu.Item>
-									{({ active }) => (
-										<button
-											onClick={onEdit}
-											className={`${
-												active ? "bg-gray-100" : ""
-											} block px-4 py-2 text-sm text-gray-700 w-full text-left`}
-										>
-											Editar
-										</button>
-									)}
-								</Menu.Item>
-								<Menu.Item>
-									{({ active }) => (
-										<button
-											onClick={handleDelete}
-											className={`${
-												active ? "bg-gray-100" : ""
-											} block px-4 py-2 text-sm text-gray-700 w-full text-left`}
-										>
-											Excluir
-										</button>
-									)}
-								</Menu.Item>
-							</Menu.Items>
-						</Menu>
-					</Header>
-					<Droppable droppableId={columnId} type="QUOTE">
-						{(provided: DroppableProvided, snapshot) => (
-							<QuoteListContainer
-								ref={provided.innerRef}
-								{...provided.droppableProps}
-								style={{
-									backgroundColor: snapshot.isDraggingOver
-										? colors.G50
-										: "white",
-								}}
+		<>
+			<Draggable draggableId={columnId} index={index}>
+				{(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
+					<Container ref={provided.innerRef} {...provided.draggableProps}>
+						<Header isDragging={snapshot.isDragging}>
+							<Title
+								{...provided.dragHandleProps}
+								aria-label={`${title} quote list`}
 							>
-								{quotesList.map((quote, index) => (
-									<Draggable
-										key={quote.id}
-										draggableId={quote.id}
-										index={index}
-									>
-										{(provided, snapshot) => (
-											<div
-												ref={provided.innerRef}
-												{...provided.draggableProps}
-												{...provided.dragHandleProps}
-												style={{
-													userSelect: "none",
-													padding: 16,
-													margin: "0 0 8px 0",
-													minHeight: "50px",
-													backgroundColor: snapshot.isDragging
-														? "#263B4A"
-														: "#456C86",
-													color: "white",
-													...provided.draggableProps.style,
-												}}
+								{title}
+							</Title>
+							<Menu as="div" className="absolute top-2 right-2">
+								<Menu.Button className="text-gray-500 hover:text-gray-700">
+									•••
+								</Menu.Button>
+								<Menu.Items className="absolute right-0 w-48 py-1 mt-2 bg-white border border-gray-200 rounded-md shadow-lg">
+									<Menu.Item>
+										{({ active }) => (
+											<button
+												onClick={onEdit}
+												className={`${
+													active ? "bg-gray-100" : ""
+												} block px-4 py-2 text-sm text-gray-700 w-full text-left`}
 											>
-												{quote.content}
-											</div>
+												Editar
+											</button>
 										)}
-									</Draggable>
-								))}
-								{provided.placeholder}
-							</QuoteListContainer>
-						)}
-					</Droppable>
-					<button
-						onClick={() => setIsModalOpen(true)}
-						className="bg-slate-700 text-white px-4 py-2 rounded hover:bg-slate-900 mt-2"
-					>
-						Adicionar Cartão
-					</button>
-					<AddCardForm columnId={columnId} onAddCard={onAddCard} />
-					<CreateCardModal
-						isOpen={isModalOpen}
-						onClose={() => setIsModalOpen(false)}
-						onSubmit={onAddCard}
-						priorities={priorities}
-						categories={categories}
-						statuses={statuses}
-					/>
-				</Container>
-			)}
-		</Draggable>
+									</Menu.Item>
+									<Menu.Item>
+										{({ active }) => (
+											<button
+												onClick={handleDelete}
+												className={`${
+													active ? "bg-gray-100" : ""
+												} block px-4 py-2 text-sm text-gray-700 w-full text-left`}
+											>
+												Excluir
+											</button>
+										)}
+									</Menu.Item>
+									<Menu.Item>
+										{({ active }) => (
+											<button
+												onClick={() => setIsModalOpen(true)}
+												className={`${
+													active ? "bg-gray-100" : ""
+												} block px-4 py-2 text-sm text-gray-700 w-full text-left`}
+											>
+												Adicionar Cartão
+											</button>
+										)}
+									</Menu.Item>
+								</Menu.Items>
+							</Menu>
+						</Header>
+						<Droppable droppableId={columnId} type="QUOTE">
+							{(provided: DroppableProvided, snapshot) => (
+								<QuoteListContainer
+									ref={provided.innerRef}
+									{...provided.droppableProps}
+									style={{
+										backgroundColor: snapshot.isDraggingOver
+											? colors.G50
+											: "white",
+									}}
+								>
+									{quotesList.map((quote, index) => (
+										<Draggable
+											key={quote.id}
+											draggableId={quote.id}
+											index={index}
+										>
+											{(provided, snapshot) => (
+												<div
+													ref={provided.innerRef}
+													{...provided.draggableProps}
+													{...provided.dragHandleProps}
+													style={{
+														userSelect: "none",
+														padding: 16,
+														margin: "0 0 8px 0",
+														minHeight: "50px",
+														backgroundColor: snapshot.isDragging
+															? "#263B4A"
+															: "#456C86",
+														color: "white",
+														...provided.draggableProps.style,
+													}}
+												>
+													{quote.content}
+												</div>
+											)}
+										</Draggable>
+									))}
+									{provided.placeholder}
+								</QuoteListContainer>
+							)}
+						</Droppable>
+						<AddCardForm columnId={columnId} onAddCard={onAddCard} />
+					</Container>
+				)}
+			</Draggable>
+			<NewCardModal
+				isOpen={isModalOpen}
+				onClose={() => setIsModalOpen(false)}
+				onSubmit={handleAddCard}
+			/>
+		</>
 	);
 };
 
