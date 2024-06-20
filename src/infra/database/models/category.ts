1092f53e013830167bc8f9225ee9f81babefdd6b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-20 09:32:47 -0300

    feat: Atualiza modelo de categoria para adicionar campo de descrição

--- src/infra/database/models/category.ts
+++ src/infra/database/models/category.ts
@@ -1,12 +1,11 @@
 import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
 import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const category = pgTable('category', {
-  id: uuid('id').defaultRandom().primaryKey(),
-  name: varchar("name").notNull(),
-  description: varchar("description").notNull()
+export const category = pgTable("category", {
+	id: uuid("id").defaultRandom().primaryKey(),
+	name: varchar("name").notNull(),
+	description: varchar("description").notNull(),
 });
-
 
 export type Category = InferSelectModel<typeof category>;
 export type NewCategory = InferInsertModel<typeof category>;
