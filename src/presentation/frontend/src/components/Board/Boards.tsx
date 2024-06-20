5841d2ae9c8b64cf4f9370b501f45d222f4c67b9
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-06-20 14:48:49 -0300

    feat: Atualizar rota de criação de cartões no serviço de cartões

--- src/presentation/frontend/src/components/Board/Boards.tsx
+++ src/presentation/frontend/src/components/Board/Boards.tsx
@@ -1,287 +1,86 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
-import { Global, css } from "@emotion/react";
-import { colors } from "@atlaskit/theme";
-import type {
-	DropResult,
-	DraggableLocation,
-	DroppableProvided,
-} from "@hello-pangea/dnd";
 import { DragDropContext, Droppable } from "@hello-pangea/dnd";
-import type { QuoteMap, Quote, Board as BoardType } from "@/types/board";
-import { PartialAutoScrollerOptions } from "@/components/auto-scroller/fluid-scroller/auto-scroller-options-types";
-import { reorderQuoteMap, reorder } from "@/services/reorder";
+import { colors } from "@atlaskit/theme";
 import Column from "@/components/Column/Column";
-import AddColumnForm from "../Column/AddColumnForm";
-import EditColumnModal from "../modal/NewColumnModal";
-
-interface ParentContainerProps {
-	height: string;
-}
-
-const ParentContainer = styled.div<ParentContainerProps>`
-	height: ${({ height }) => height};
-	overflow-x: hidden;
-	overflow-y: auto;
-`;
-
+import { fetchCards, createCard } from "@/services/cardService";
+import { Board, Card, NewCard } from "@/types/board";
+import { Column as ColumnType } from "@/types/column";
 const Container = styled.div`
 	background-color: ${colors.B100};
 	min-height: 100vh;
-	/* like display:flex but will allow bleeding over the window width */
 	min-width: 100vw;
 	display: inline-flex;
 `;
 
 interface Props {
-	initial: BoardType;
-	withScrollableColumns?: boolean;
-	isCombineEnabled?: boolean;
-	containerHeight?: string;
-	useClone?: boolean;
-	applyGlobalStyles?: boolean;
-	autoScrollerOptions?: PartialAutoScrollerOptions;
-	priorities: string[];
-	categories: string[];
-	statuses: string[];
+	initialBoard: Board;
 }
 
-const Board: React.FC<Props> = ({
-	initial,
-	withScrollableColumns = false,
-	isCombineEnabled = false,
-	containerHeight,
-	useClone = false,
-	applyGlobalStyles = true,
-	autoScrollerOptions,
-	priorities,
-	categories,
-	statuses,
-}) => {
-	const [columns, setColumns] = useState<QuoteMap>(
-		initial.column.reduce((acc, col) => {
-			if (!col.deleted) {
-				acc[col.id] = {
-					title: col.name,
-					quotes: col.quotes.filter((quote) => !quote.deleted),
-					deleted: col.deleted,
-				};
-			}
-			return acc;
-		}, {} as QuoteMap)
-	);
+const BoardComponent: React.FC<Props> = ({ initialBoard }) => {
+	const [columns, setColumns] = useState<ColumnType[]>(initialBoard.column);
+	const [cards, setCards] = useState<Card[]>([]);
 
-	const [ordered, setOrdered] = useState<string[]>(
-		initial.column.filter((col) => !col.deleted).map((col) => col.id)
-	);
+	const fetchAllCards = async () => {
+		const fetchedCards = await fetchCards();
+		setCards(fetchedCards);
+	};
 
 	useEffect(() => {
-		setColumns(
-			initial.column.reduce((acc, col) => {
-				if (!col.deleted) {
-					acc[col.id] = {
-						title: col.name,
-						quotes: col.quotes.filter((quote) => !quote.deleted),
-						deleted: col.deleted,
-					};
-				}
-				return acc;
-			}, {} as QuoteMap)
-		);
+		fetchAllCards();
+	}, []);
 
-		setOrdered(
-			initial.column.filter((col) => !col.deleted).map((col) => col.id)
-		);
-	}, [initial.column]);
-
-	const [isEditingColumn, setIsEditingColumn] = useState<boolean>(false);
-	const [editingColumnId, setEditingColumnId] = useState<string | null>(null);
-	const [editingColumnTitle, setEditingColumnTitle] = useState<string>("");
-
-	const handleColumnTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setEditingColumnTitle(e.target.value);
-	};
-
-	const handleColumnTitleSave = () => {
-		if (editingColumnId && editingColumnTitle.trim() !== "") {
-			setColumns((prevColumns) => ({
-				...prevColumns,
-				[editingColumnId]: {
-					...prevColumns[editingColumnId],
-					title: editingColumnTitle,
-				},
-			}));
-			setIsEditingColumn(false);
-			setEditingColumnId(null);
-			setEditingColumnTitle("");
+	const handleAddCard = async (columnId: string, newCard: NewCard) => {
+		if (!newCard.idPriority || !newCard.idStatus || !newCard.title) {
+			console.error("Missing required fields");
+			return;
 		}
-	};
-
-	const handleColumnTitleCancel = () => {
-		setIsEditingColumn(false);
-		setEditingColumnId(null);
-		setEditingColumnTitle("");
-	};
-
-	const handleAddColumn = (title: string) => {
-		const newColumnId = `column-${Date.now()}`;
-		setColumns((prevColumns) => ({
-			...prevColumns,
-			[newColumnId]: {
-				title,
-				quotes: [],
+		try {
+			const response = await createCard({
+				...newCard,
+				idCategory: columnId,
+				idPriority: newCard.idPriority,
+				idStatus: newCard.idStatus,
+				createdAt: new Date().toISOString(),
 				deleted: false,
-			},
-		}));
-		setOrdered((prevOrdered) => [...prevOrdered, newColumnId]);
+			});
+			setCards((prevCards) => [...prevCards, response]);
+		} catch (error) {
+			console.error("Failed to create card", error);
+		}
 	};
-
-	const handleEditColumn = useCallback(
-		(columnId: string, columnTitle: string) => {
-			setIsEditingColumn(true);
-			setEditingColumnId(columnId);
-			setEditingColumnTitle(columnTitle);
-		},
-		[]
-	);
-
-	const handleDeleteColumn = (columnId: string) => {
-		setColumns((prevColumns) => {
-			const newColumns = { ...prevColumns };
-			newColumns[columnId].deleted = true;
-			return newColumns;
-		});
-
-		setOrdered((prevOrdered) => prevOrdered.filter((id) => id !== columnId));
-	};
-
-	const onDragEnd = (result: DropResult): void => {
-		if (result.combine) {
-			if (result.type === "COLUMN") {
-				const shallow: string[] = [...ordered];
-				shallow.splice(result.source.index, 1);
-				setOrdered(shallow);
-				return;
-			}
-
-			const column: Quote[] = columns[result.source.droppableId].quotes;
-			const withQuoteRemoved: Quote[] = [...column];
-			withQuoteRemoved.splice(result.source.index, 1);
-			setColumns((prevColumns) => ({
-				...prevColumns,
-				[result.source.droppableId]: {
-					...prevColumns[result.source.droppableId],
-					quotes: withQuoteRemoved,
-				},
-			}));
-			return;
-		}
-
-		// dropped nowhere
-		if (!result.destination) {
-			return;
-		}
-
-		const source: DraggableLocation = result.source;
-		const destination: DraggableLocation = result.destination;
-
-		// did not move anywhere - can bail early
-		if (
-			source.droppableId === destination.droppableId &&
-			source.index === destination.index
-		) {
-			return;
-		}
-
-		// reordering column
-		if (result.type === "COLUMN") {
-			const newOrder: string[] = reorder(
-				ordered,
-				source.index,
-				destination.index
-			);
-
-			setOrdered(newOrder);
-			return;
-		}
-
-		const data = reorderQuoteMap({
-			quoteMap: columns,
-			source,
-			destination,
-		});
-
-		setColumns(data.quoteMap);
-	};
-
-	const board = (
-		<Droppable
-			droppableId="board"
-			type="COLUMN"
-			direction="horizontal"
-			ignoreContainerClipping={Boolean(containerHeight)}
-			isCombineEnabled={isCombineEnabled}
-		>
-			{(provided: DroppableProvided) => (
-				<Container ref={provided.innerRef} {...provided.droppableProps}>
-					{ordered
-						.filter((key) => !columns[key].deleted)
-						.map((key: string, index: number) => (
-							<Column
-								key={key}
-								index={index}
-								columnId={key}
-								title={columns[key].title}
-								quotes={columns[key]?.quotes || []}
-								isScrollable={withScrollableColumns}
-								isCombineEnabled={isCombineEnabled}
-								useClone={useClone}
-								onAddCard={(columnId, card) => {
-									// Logic to add card to column
-								}}
-								onEdit={() => handleEditColumn(key, columns[key].title)}
-								onDelete={() => handleDeleteColumn(key)}
-							/>
-						))}
-
-					{provided.placeholder}
-					<AddColumnForm onAddColumn={handleAddColumn} boardId={initial.id} />
-				</Container>
-			)}
-		</Droppable>
-	);
 
 	return (
-		<React.Fragment>
-			<DragDropContext
-				onDragEnd={onDragEnd}
-				autoScrollerOptions={autoScrollerOptions}
-			>
-				{containerHeight ? (
-					<ParentContainer height={containerHeight}>{board}</ParentContainer>
-				) : (
-					board
+		<DragDropContext
+			onDragEnd={(result) => {
+				
+			}}
+		>
+			<Droppable droppableId="board" type="COLUMN" direction="horizontal">
+				{(provided) => (
+					<Container ref={provided.innerRef} {...provided.droppableProps}>
+						{columns.map((column, index) => (
+							<Column
+								key={column.id}
+								columnId={column.id}
+								title={column.name}
+								quotes={cards.filter((card) => card.idCategory === column.id)} // Filtrar os cartões pela coluna correspondente
+								index={index}
+								onAddCard={handleAddCard}
+								onEdit={() => {
+									// Lógica de edição aqui
+								}}
+								onDelete={() => {
+									// Lógica de exclusão aqui
+								}}
+							/>
+						))}
+						{provided.placeholder}
+					</Container>
 				)}
-			</DragDropContext>
-			{applyGlobalStyles ? (
-				<Global
-					styles={css`
-						body {
-							background: ${colors.B200};
-						}
-					`}
-				/>
-			) : null}
-
-			<EditColumnModal
-				isOpen={isEditingColumn}
-				onClose={handleColumnTitleCancel}
-				onSubmit={handleColumnTitleSave}
-				columnId={editingColumnId || ""}
-				initialTitle={editingColumnTitle}
-			/>
-		</React.Fragment>
+			</Droppable>
+		</DragDropContext>
 	);
 };
 
-export default Board;
+export default BoardComponent;
